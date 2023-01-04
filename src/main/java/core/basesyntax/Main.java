ee5ffc56702a93a19838d2887c4bb037adeae3e2
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2023-01-04 20:36:43 +0200

    Compleated thread-pool task

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,7 +2,11 @@ package core.basesyntax;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +14,17 @@ public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        MyThread thread = new MyThread();
         List<Future<String>> futures = new ArrayList<>();
-        // write your code here
+        IntStream.range(0, 20).forEach(value -> futures.add(executor.submit(thread)));
+        executor.shutdown();
+        futures.forEach(f -> {
+            try {
+                logger.info(f.get());
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
--- src/main/java/core/basesyntax/MyThread.java
+++ src/main/java/core/basesyntax/MyThread.java
@@ -1,11 +1,22 @@
 package core.basesyntax;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class MyThread implements Callable<String> {
+    private static final String TIME_FORMAT = "H:mm:ss.SSSSSS";
+    private static final int START_DURATION = 100;
+    private static final int ZERO = 0;
+    private static final int BOUND = 5;
+
     @Override
     public String call() throws InterruptedException {
-        // write your code here
-        return null;
+        Random random = new Random();
+        int multiplier = random.nextInt(BOUND);
+        int duration = multiplier == ZERO ? START_DURATION : START_DURATION * multiplier;
+        String time = LocalTime.now().format(DateTimeFormatter.ofPattern(TIME_FORMAT));
+        return String.format("Task duration was %d ms, execution finished at %s", duration, time);
     }
 }
